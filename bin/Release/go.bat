4161badb5329d45fc849a87b6cc7ec2ae96fe16c
John W. Ratcliff <jratcliffscarab@gmial.com>
2023-07-07 15:50:10 -0600

    batch file

---
+++ bin/Release/go.bat
@@ -0,0 +1,3 @@
+SchemaCodeGen d:\github\SchemaCodeGen\bitcoinstats.csv d:\bitcoinstats
+copy d:\bitcoinstats\bitcoinstats.h d:\github\bitcoinstats\include
+copy d:\bitcoinstats\bitcoinstats.cpp d:\github\bitcoinstats\src
